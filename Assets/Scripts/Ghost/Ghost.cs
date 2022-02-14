0a517b2b2d8255aaa02e5afa1494993b4a0b2ab6
Lee Sun Jae <lsj010320@gmail.com>
2022-02-14 23:35:20 +0900

    Add Ghost Script

---
+++ Assets/Scripts/Ghost/Ghost.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ghost : MonoBehaviour
+{
+    [SerializeField]
+    protected int speed;
+
+    private Transform playerTr;
+    private Transform ghostTr;
+
+    public void Start()
+    {
+        playerTr = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        ghostTr = GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        ChaseTarget(playerTr.position); 
+    }
+
+    private void ChaseTarget(Vector3 target)
+    {
+        Vector3 movedir = target - ghostTr.position;
+        ghostTr.Translate(movedir.normalized * Time.deltaTime * speed, Space.Self);
+    }
+}
